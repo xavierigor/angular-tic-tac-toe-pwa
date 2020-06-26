ed66d1dc0b7aeeef44f841f86d4d217211d1c0a3
Igor Xavier <igoorlimaa@gmail.com>
2020-06-26 12:45:12 -0300

    feat: add logic for board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,10 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame()
   }
 
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  // returns which player is next
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(index: number) {
+    if (!this.squares[index]) {
+      this.squares.splice(index, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+  }
+
+  // algorithm to tell if a player won
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
 }
